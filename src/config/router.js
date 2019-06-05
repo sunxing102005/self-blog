112499d765ba924f7ac15a610a0949bd1df8e238
xing.sun <xing.sun@nio.com>
2019-06-05 20:46:18 +0800

    add  router

--- src/config/router.js
+++ src/config/router.js
@@ -1,8 +1,8 @@
 module.exports = [
     // RESTFUL
-    // [/metrics\/(.*)/i, "metrics/index"],
-    // [/\/api\/(\w+)(?:\/(.*))?/, "api/:1?id=:2", "rest"],
-    // [/\/font\/(\w+)\/(\w+)/, "fontend/:1/:2"],
-    // ["/:category/:slug", "content/detail"],
-    // ["/:category/:slug/comment", "content/comment"]
+    [/metrics\/(.*)/i, "metrics/index"],
+    [/\/api\/(\w+)(?:\/(.*))?/, "api/:1?id=:2", "rest"],
+    [/\/font\/(\w+)\/(\w+)/, "fontend/:1/:2"],
+    ["/:category/:slug", "content/detail"],
+    ["/:category/:slug/comment", "content/comment"]
 ];
